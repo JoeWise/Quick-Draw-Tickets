643aadd65791e42fb150fbf33462e0c6de836eef
Joe Wise <joedavidwise@gmail.com>
2025-07-11 20:08:56 -0700

    Change code styling

--- backend/src/models/userModel.ts
+++ backend/src/models/userModel.ts
@@ -2,23 +2,26 @@ import db from '../utils/db';
 import { QueryResult } from 'pg';
 import { User } from '../types/User';
 
-export async function createUser(email: string, passwordHash: string): Promise<Pick<User, 'id' | 'email'>> {
-  const result: QueryResult<Pick<User, 'id' | 'email'>> = await db.query(
-    'INSERT INTO users (email, password_hash) VALUES ($1, $2) RETURNING id, email',
-    [email, passwordHash]
-  );
-  return result.rows[0];
+export async function createUser(email: string, passwordHash: string): Promise<Pick<User, 'id' | 'email'>> 
+{
+    const result: QueryResult<Pick<User, 'id' | 'email'>> = await db.query(
+        'INSERT INTO users (email, password_hash) VALUES ($1, $2) RETURNING id, email',
+        [email, passwordHash]
+    );
+    return result.rows[0];
 }
 
-export async function findUserByEmail(email: string): Promise<User | undefined> {
-  const result: QueryResult<User> = await db.query('SELECT * FROM users WHERE email = $1', [email]);
-  return result.rows[0];
+export async function findUserByEmail(email: string): Promise<User | undefined> 
+{
+    const result: QueryResult<User> = await db.query('SELECT * FROM users WHERE email = $1', [email]);
+    return result.rows[0];
 }
 
-export async function findUserById(id: number): Promise<Pick<User, 'id' | 'email'> | undefined> {
-  const result: QueryResult<Pick<User, 'id' | 'email'>> = await db.query(
-    'SELECT id, email FROM users WHERE id = $1',
-    [id]
-  );
-  return result.rows[0];
+export async function findUserById(id: number): Promise<Pick<User, 'id' | 'email'> | undefined> 
+{
+    const result: QueryResult<Pick<User, 'id' | 'email'>> = await db.query(
+        'SELECT id, email FROM users WHERE id = $1',
+        [id]
+    );
+    return result.rows[0];
 }
