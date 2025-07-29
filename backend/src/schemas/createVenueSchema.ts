8c6b64ec164846d1e07d575837695015e4096c2f
Joe Wise <joedavidwise@gmail.com>
2025-07-28 18:14:22 -0700

    Remove trailing comma

--- backend/src/schemas/createVenueSchema.ts
+++ backend/src/schemas/createVenueSchema.ts
@@ -2,5 +2,5 @@ import { z } from 'zod';
 
 export const createVenueSchema = z.object({
     name: z.string().min(1),
-    address: z.string().min(1).optional(),
+    address: z.string().min(1).optional()
 });
